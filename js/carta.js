677525206dd50b61c41f18db2d4f93f52c9824c7
Alberto Blasco <albertoblascoo@gmail.com>
2026-07-16 02:11:50 +0200

    a

--- js/carta.js
+++ js/carta.js
@@ -2,7 +2,6 @@ const container = document.getElementById("menu-container");
 const API = window.location.hostname === "localhost"? "http://localhost:3000/api": "/api";
 
 console.log("idioma: "+idioma);
-//router.get("/",  cartaController.obtenerCarta);
 cargarCarta(idioma);
 
 async function cargarCarta(idioma){
@@ -37,35 +36,3 @@ async function cargarCarta(idioma){
 
 });
 }
-/*
-datos.forEach(categoria=>{
-    container.innerHTML += `
-
-        <section class="categoria">
-
-            <h2>${categoria.categoria}</h2>
-
-            ${categoria.platos.map(plato=>`
-
-                <article class="plato">
-
-                    <div>
-
-                        <h3>${plato.nombre}</h3>
-
-                        <p>${plato.descripcion}</p>
-
-                    </div>
-
-                    <span>${plato.precio}</span>
-
-                </article>
-
-            `).join("")}
-
-        </section>
-
-    `;
-
-});
-*/
